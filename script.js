32999ec04a4db56f1a8fb42d022c232a36d809de
bars <6ars1k009@gmail.com>
2019-05-31 17:49:01 +0300

    test 3 html/css

---
+++ script.js
@@ -0,0 +1,12 @@
+$(function() {
+  // Owl Carousel
+  var owl = $(".owl-carousel");
+  owl.owlCarousel({
+    items: 1,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    smartSpeed: 1000
+
+  });
+});
